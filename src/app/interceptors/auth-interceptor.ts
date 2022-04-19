577a2be0c949b3036f61f67a21a2abdec51a5c53
matheus.dimer <matheus.dimer@betha.com.br>
2022-04-18 21:06:47 -0300

    Interceptor de erro.

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,19 +1,29 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import {
+  HttpErrorResponse,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { tap } from "rxjs";
+import { AuthService } from "../services/auth.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private notification: ToastrService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    const authToken = this.auth.token || '';
+    const authToken = this.auth.token || "";
 
     const authReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${authToken}`)
+      headers: req.headers.set("Authorization", `Bearer ${authToken}`),
     });
 
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(tap({ error: this.handleError.bind(this) }));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    this.notification.error(error.error.message || error.message);
   }
 }
