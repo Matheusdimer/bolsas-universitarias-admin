a7e81b804e6f8cd00b0525551bb6cd91c1259e2d
matheus.dimer <matheus.dimer@betha.com.br>
2022-04-18 23:24:03 -0300

    Loading com skeleton para edição de bolsas.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,2 @@
-export const apiUrl = 'http://localhost:8090/api'
-  //'https://bolsas-universitarias-api.herokuapp.com/api'
+export const apiUrl = //'http://localhost:8090/api'
+  'https://bolsas-universitarias-api.herokuapp.com/api'
